aa0478bfb917918f60edf100520035c5afca48d0
MHuiG <616001163@qq.com>
2020-04-30 13:20:28 +0800

    Update me.js

--- js/me.js
+++ js/me.js
@@ -2,12 +2,14 @@
 $("figcaption").click(function(){
   if(window.CodeBlockFullscreen){
 	  $("#post").removeClass("code-block-fullscreen");
+	  $(".post").removeClass("code-block-fullscreen");
       $(this).parent().removeClass("code-block-fullscreen");
       $(this).parent().removeClass("code-block-fullscreen-overflow-auto");
       $("html").removeClass("code-block-fullscreen-html-scroll");
 	  window.CodeBlockFullscreen=false
   }else{
 	  $("#post").addClass("code-block-fullscreen");
+	  $(".post").addClass("code-block-fullscreen");
       $(this).parent().addClass("code-block-fullscreen");
       $(this).parent().addClass("code-block-fullscreen-overflow-auto");
       $("html").addClass("code-block-fullscreen-html-scroll");
