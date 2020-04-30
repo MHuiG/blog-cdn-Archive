051db2bf064b306ab676c0521a2e8b4ed3af0626
MHuiG <616001163@qq.com>
2020-04-30 12:05:04 +0800

    code-block-fullscreen

--- js/me.js
+++ js/me.js
@@ -1,4 +1,16 @@
-/*mac*/
+/*code-block-fullscreen*/
 $("figcaption").click(function(){
-  console.log(1)
+  if(window.CodeBlockFullscreen){
+	  $("#post").removeClass("code-block-fullscreen");
+      $(this).parent().removeClass("code-block-fullscreen");
+      $(this).parent().removeClass("code-block-fullscreen-overflow-auto");
+      $("html").removeClass("code-block-fullscreen-html-scroll");
+	  window.CodeBlockFullscreen=false
+  }else{
+	  $("#post").addClass("code-block-fullscreen");
+      $(this).parent().addClass("code-block-fullscreen");
+      $(this).parent().addClass("code-block-fullscreen-overflow-auto");
+      $("html").addClass("code-block-fullscreen-html-scroll");
+	  window.CodeBlockFullscreen=true
+  }
 });
