187fbcf974af7fdf6c3cb00328e2319acefd1239
MHuiG <616001163@qq.com>
2020-04-30 13:46:42 +0800

    code-block-fullscreen

--- js/me.js
+++ js/me.js
@@ -2,14 +2,12 @@
 $("figcaption").click(function(){
   if(window.CodeBlockFullscreen){
 	  $("#post").removeClass("code-block-fullscreen");
-	  $(".post").removeClass("code-block-fullscreen");
       $(this).parent().removeClass("code-block-fullscreen");
       $(this).parent().removeClass("code-block-fullscreen-overflow-auto");
       $("html").removeClass("code-block-fullscreen-html-scroll");
 	  window.CodeBlockFullscreen=false
   }else{
 	  $("#post").addClass("code-block-fullscreen");
-	  $(".post").addClass("code-block-fullscreen");
       $(this).parent().addClass("code-block-fullscreen");
       $(this).parent().addClass("code-block-fullscreen-overflow-auto");
       $("html").addClass("code-block-fullscreen-html-scroll");
