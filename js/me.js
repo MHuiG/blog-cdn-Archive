a7210f75c8cb182e361a3d6eb8c53aba0efaa519
MHuiG <616001163@qq.com>
2020-05-01 16:35:43 +0800

    code-block-fullscreen-gutter

--- js/me.js
+++ js/me.js
@@ -5,12 +5,14 @@ $("figcaption").click(function(){
       $(this).parent().removeClass("code-block-fullscreen");
       $(this).parent().removeClass("code-block-fullscreen-overflow-auto");
       $("html").removeClass("code-block-fullscreen-html-scroll");
+      $(".highlight>table .gutter").removeClass("code-block-fullscreen-gutter");
 	  window.CodeBlockFullscreen=false
   }else{
 	  $("#post").addClass("code-block-fullscreen");
       $(this).parent().addClass("code-block-fullscreen");
       $(this).parent().addClass("code-block-fullscreen-overflow-auto");
       $("html").addClass("code-block-fullscreen-html-scroll");
+      $(".highlight>table .gutter").addClass("code-block-fullscreen-gutter");
 	  window.CodeBlockFullscreen=true
   }
 });
