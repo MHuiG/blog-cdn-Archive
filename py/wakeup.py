3065187d7b2fcc8498bef9f4da03974d3c92476b
MHuiG <616001163@qq.com>
2020-08-07 09:33:10 +0800

    up

---
+++ py/wakeup.py
@@ -0,0 +1,7 @@
+import os
+
+d = os.popen("curl https://mhuig.avosapps.us")
+d = os.popen("curl https://s1uxuqjzzpqr.leanapp.cn")
+d = os.popen("curl https://zhm0aoiqwqwt.leanapp.cn")
+
+print("Done")
