37ae9a016c4becb85968c9a2db73b25070aa28aa
H <616001163@qq.com>
2020-08-31 09:20:44 +0800

    Update wakeup.py

--- py/wakeup.py
+++ py/wakeup.py
@@ -1,7 +1,6 @@
 import os
 
 d = os.popen("curl https://mhuig.avosapps.us")
-d = os.popen("curl https://s1uxuqjzzpqr.leanapp.cn")
-d = os.popen("curl https://zhm0aoiqwqwt.leanapp.cn")
+d = os.popen("curl https://minivaline.avosapps.us")
 
 print("Done")
